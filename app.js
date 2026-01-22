cbe609f1c0ca876a112b65504311083c3f1bf300
@hariprasath_7 <hariprasath.m@grouppal.in>
2026-01-23 01:27:08 +0530

    updated name on app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 const PORT = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello, World! Hi HariPrasath');
+  res.send('Hello, World! Hi Vijay');
 });
 
 app.listen(PORT, () => {
